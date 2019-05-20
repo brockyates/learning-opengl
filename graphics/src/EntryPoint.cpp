452f01ad98f890f3008e3414135e4861f0cbcd92
Brock Yates <brock.e.yates@gmail.com>
2019-05-20 11:04:11 -0700

    Entry point cleanup

--- graphics/src/EntryPoint.cpp
+++ graphics/src/EntryPoint.cpp
@@ -1,17 +1,13 @@
 #include "pch.h"
 #include "Application.h"
 
+//Hide console window
 #pragma comment(linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")
 
 int main()
 {
     Graphics::Utils::Log::Init();
-
-    Graphics::Application application;
-
-    {
-        application.Start();
-    }
+    Graphics::Application().Start();
 
     return 0;
 }
