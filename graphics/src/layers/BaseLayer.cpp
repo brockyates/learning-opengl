582089b21a2c82dafe95df93931805aad53f1bb7
Brock Yates <brock.e.yates@gmail.com>
2019-05-25 15:13:51 -0700

    Baselayer: small cleanup

--- graphics/src/layers/BaseLayer.cpp
+++ graphics/src/layers/BaseLayer.cpp
@@ -183,10 +183,10 @@ namespace Graphics {
         ImGui::SetNextWindowViewport(viewport->ID);
         ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
         ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
         window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
         window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
 
-        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
         ImGui::Begin("MainDockspaceWindow", 0, window_flags);
         ImGui::PopStyleVar(3);
 
