73ac8c1139c3d4d0748fa2a69638b6895ed6ff97
Brock Yates <brock.e.yates@gmail.com>
2019-05-28 05:58:52 -0700

    Fix triangle properties: equaliteral triangle

--- graphics/src/models/Triangle.h
+++ graphics/src/models/Triangle.h
@@ -11,20 +11,23 @@ namespace Graphics {
 
     struct Triangle : public Model
     {
+    private:
+        static constexpr float yOffset = 0.866f;
+
     public:
         Triangle()
             : Model(
                 {
                     {
-                        glm::vec4{ 0.0f,  1.0f, 0.0f, 1.0f }, //Position
+                        glm::vec4{ 0.0f,  yOffset, 0.0f, 1.0f }, //Position
                         glm::vec4{ 1.0f,  0.0f, 0.0f, 1.0f }, //Color
                     },
                     {
-                        glm::vec4{ 1.0f, -1.0f, 0.0f, 1.0f },
+                        glm::vec4{ 1.0f, -yOffset,  0.0f, 1.0f },
                         glm::vec4{ 0.0f,  1.0f, 0.0f, 1.0f },
                     },
                     {
-                        glm::vec4{-1.0f, -1.0f, 0.0f, 1.0f },
+                        glm::vec4{ -1.0f, -yOffset, 0.0f, 1.0f },
                         glm::vec4{ 0.0f,  0.0f, 1.0f, 1.0f },
                     } //Vertex data
                 },
