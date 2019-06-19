61ebe37202c5bced2de8881e0d4b265cf7f3c6e2
Brock Yates <brock.e.yates@gmail.com>
2019-06-18 22:27:52 -0700

    Add ModelGenerator tests

--- graphics.unittests/src/ModelGeneratorTests.cpp
+++ graphics.unittests/src/ModelGeneratorTests.cpp
@@ -11,3 +11,22 @@ TEST_CASE("ModelGenerator sanity test", "[ModelGenerator]")
 
     REQUIRE(!std::empty(model->Vertexes));
 }
+
+TEST_CASE("ModelGenerator triangle vertex count", "[ModelGenerator]")
+{
+    const auto model = ModelGenerator::MakeTriangle();
+    const auto expectedVertexCount = 3;
+
+    REQUIRE(std::size(model->Vertexes) == expectedVertexCount);
+}
+
+TEST_CASE("ModelGenerator triangle vertex data byte size ", "[ModelGenerator]")
+{
+    const auto model = ModelGenerator::MakeTriangle();
+
+    // 1 vertex = 4 position floats, 4 color floats = 8 floats * 4 bytes / float = 32 bytes / vertex
+    // 32 bytes / vertex * 3 vertexes = 96 bytes
+    const auto expectedVertexByteSize = 96;
+
+    REQUIRE(model->VertexDataByteSize() == expectedVertexByteSize);
+}
