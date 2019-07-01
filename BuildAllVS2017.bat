a5c43a43a3fa684b8ae105f1fbba875badcf3733
Brock Yates <brock.e.yates@gmail.com>
2019-06-30 17:58:52 -0700

    Improve Build script

--- BuildAllVS2017.bat
+++ BuildAllVS2017.bat
@@ -1,9 +1,9 @@
-call vendor\bin\premake\premake5.exe vs2017
-msbuild.exe Graphics.sln /t:Clean /p:Configuration=Debug
-msbuild.exe Graphics.sln /t:Clean /p:Configuration=Release
+call %~dp0vendor\bin\premake\premake5.exe vs2017
+msbuild.exe %~dp0Graphics.sln /t:Clean /p:Configuration=Debug
+msbuild.exe %~dp0Graphics.sln /t:Clean /p:Configuration=Release
 
-msbuild.exe Graphics.sln /t:Rebuild /p:Configuration=Debug
-msbuild.exe Graphics.sln /t:Rebuild /p:Configuration=Release
+msbuild.exe %~dp0Graphics.sln /t:Rebuild /p:Configuration=Debug
+msbuild.exe %~dp0Graphics.sln /t:Rebuild /p:Configuration=Release
 
-bin\Debug-windows-x86_64\Graphics.UnitTests\Graphics.UnitTests.exe
-bin\Release-windows-x86_64\Graphics.UnitTests\Graphics.UnitTests.exe
+%~dp0bin\Debug-windows-x86_64\Graphics.UnitTests\Graphics.UnitTests.exe
+%~dp0bin\Release-windows-x86_64\Graphics.UnitTests\Graphics.UnitTests.exe
