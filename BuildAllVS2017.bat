7d04cf839b3fc6bcd3a7f86aa71b886abd776e53
Brock Yates <brock.e.yates@gmail.com>
2019-06-30 18:04:06 -0700

    Fix and rename Build script

--- BuildAllVS2017.bat
+++
@@ -1,9 +0,0 @@
-call %~dp0vendor\bin\premake\premake5.exe vs2017
-msbuild.exe %~dp0Graphics.sln /t:Clean /p:Configuration=Debug
-msbuild.exe %~dp0Graphics.sln /t:Clean /p:Configuration=Release
-
-msbuild.exe %~dp0Graphics.sln /t:Rebuild /p:Configuration=Debug
-msbuild.exe %~dp0Graphics.sln /t:Rebuild /p:Configuration=Release
-
-%~dp0bin\Debug-windows-x86_64\Graphics.UnitTests\Graphics.UnitTests.exe
-%~dp0bin\Release-windows-x86_64\Graphics.UnitTests\Graphics.UnitTests.exe
---
+++ BuildVS2017.bat
@@ -0,0 +1,10 @@
+cd %~dp0
+call vendor\bin\premake\premake5.exe vs2017
+msbuild.exe Graphics.sln /t:Clean /p:Configuration=Debug
+msbuild.exe Graphics.sln /t:Clean /p:Configuration=Release
+
+msbuild.exe Graphics.sln /t:Rebuild /p:Configuration=Debug
+msbuild.exe Graphics.sln /t:Rebuild /p:Configuration=Release
+
+bin\Debug-windows-x86_64\Graphics.UnitTests\Graphics.UnitTests.exe
+bin\Release-windows-x86_64\Graphics.UnitTests\Graphics.UnitTests.exe
